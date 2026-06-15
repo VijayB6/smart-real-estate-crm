626201dbc6e442ed48ca9c89f7854ae6d1d9f9f5
VijayB6 <vijayalayan7777@gmail.com>
2026-06-15 16:10:08 +0530

    Connected frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://smart-real-estate-crm.onrender.com/api",
 });
 
 export default API;
